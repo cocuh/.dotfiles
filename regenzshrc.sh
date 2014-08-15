ff261e0b3dc1ae121d04a1d682bc3e86c8685105
cocu <cocu.436f6375@gmail.com>
2014-08-16 01:46:08 +0900

    add regenzshrc.sh

---
+++ regenzshrc.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ -e ~/.zshrc ];then
+    rm ~/.zshrc
+fi
+for filename in $(find ~/.zshrc.d/ -type f | sort)
+do
+    echo \#$(basename $filename) >> ~/.zshrc
+    cat $filename >> ~/.zshrc
+    echo >> ~/.zshrc 
+done
+echo regened zshrc
+source ~/.zshrc
