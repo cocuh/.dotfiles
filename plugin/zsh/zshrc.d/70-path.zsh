9b1ab8974f8b099634c9383575c5f4ceda1ac37e
cocu <cocu.436f6375@gmail.com>
2014-09-15 20:36:55 +0900

    fix path

--- plugin/zsh/zshrc.d/70-path.zsh
+++ plugin/zsh/zshrc.d/70-path.zsh
@@ -1,21 +1,22 @@
 # path
 binPathArrayBefore=(\
-    "$HOME/.rbenv/bin",\
+    "$HOME/.rbenv/bin"\
 )
 binPathArrayAfter=(\
-    "$HOME/bin/bin",\
-    "$HOME/bin/bin-git/utils",\
-    "$HOME/bin/bin-git/twitter",\
-    "$HOME/bin/bin-git/utils",\
-    "$HOME/bin/utils",\
-    "$HOME/bin/coins-tools",\
-    "$HOME/bin/coins-utils",\
-    "$HOME/.local/bin",\
+    "$HOME/bin/bin"\
+    "$HOME/bin/bin-git/utils"\
+    "$HOME/bin/bin-git/twitter"\
+    "$HOME/bin/bin-git/utils"\
+    "$HOME/bin/utils"\
+    "$HOME/bin/coins-tools"\
+    "$HOME/bin/coins-utils"\
+    "$HOME/.local/bin"\
+    "$HOME/.gem/ruby/2.1.0/bin"\
 )
 
 for binPath in "${binPathArrayBefore[@]}";do;
     if [ -d $binPath ]; then
-        export PATH=$PATH:$binPath
+        export PATH=$binPath:$PATH
     fi
 done
 
