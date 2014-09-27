c1fbcc1fa3f20ff168863f6e136d6f3c6241e3ea
cocu <cocu.436f6375@gmail.com>
2014-09-28 02:10:51 +0900

    fix java antiaising

--- plugin/zsh/zshrc.d/95-java.zsh
+++ plugin/zsh/zshrc.d/95-java.zsh
@@ -1,1 +1,2 @@
-export _JAVA_OPTIONS=-Dawt.useSystemAAFontSettings=true
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dsun.java2d.xrender=true'
+export JAVA_FONTS='/usr/share/fonts/TTF'
