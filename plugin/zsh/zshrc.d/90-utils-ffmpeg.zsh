a738be9a10e64a7a98e6a215cc3819734f6506f1
cocu <cocu.436f6375@gmail.com>
2014-11-17 21:32:19 +0900

    add symmetory option to ffmpeg-conv scripts

--- plugin/zsh/zshrc.d/90-utils-ffmpeg.zsh
+++ plugin/zsh/zshrc.d/90-utils-ffmpeg.zsh
@@ -1,21 +1,76 @@
 
 # ffmpeg
 function ffmpeg-conv2ogg(){
+    cmd=$0
+    Usage() {
+        echo "USAGE: $cmd [-s] input output"
+    }
+    inputfile= outputfile= symmetry=false
+
+    while getopts s opt
+    do
+        case "$opt" in
+            "s") symmetry=true;;
+        esac
+    done
+
+    shift $((OPTIND -1))
+
     case $# in
-        2 )
-            ffmpeg -i $1 -map_metadata -1 -acodec libvorbis -vn -ab 256k $2
+        1 )
+            if ! $symmetry; then
+                Usage
+                return 1
+            fi
+            inputfile=$1
+            outputfile=${inputfile%.*}.ogg
             ;;
-        * ) echo "USAGE: $0 input output"
+        2 )
+            inputfile=$1
+            outputfile=$2
+            ;;
+        * ) 
+            Usage
+            return 1
             ;;
     esac
+    ffmpeg -i $inputfile -map_metadata -1 -acodec libvorbis -vn -ab 256k $outputfile
 }
 
 function ffmpeg-conv2mp3(){
+    cmd=$0
+    Usage() {
+        echo "USAGE: $cmd [-s] input output"
+        return 1
+    }
+    inputfile= outputfile= symmetry=false
+
+    while getopts s opt
+    do
+        case "$opt" in
+            "s") symmetry=true;;
+        esac
+    done
+
+    shift $((OPTIND -1))
+
     case $# in
-        2 )
-            ffmpeg -i $1 -map_metadata -1 -acodec mp3 -vn $2
+        1 )
+            if ! $symmetry; then
+                Usage
+                return 1
+            fi
+            inputfile=$1
+            outputfile=${inputfile%.*}.mp3
             ;;
-        * ) echo "USAGE: $0 input output"
+        2 )
+            inputfile=$1
+            outputfile=$2
+            ;;
+        * ) 
+            Usage
+            return 1
             ;;
     esac
+    ffmpeg -i $inputfile -map_metadata -1 -acodec mp3 -vn -ab 256k $outputfile
 }
