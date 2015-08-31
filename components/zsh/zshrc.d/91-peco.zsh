3a77a4a1115ed77ee8222e0872538985f628f379
Kosuke Kusano <cocuh.kk@gmail.com>
2015-08-31 09:40:54 +0900

    escape when ls

--- components/zsh/zshrc.d/91-peco.zsh
+++ components/zsh/zshrc.d/91-peco.zsh
@@ -4,7 +4,6 @@ export EDITOR=vim # 好きなエディタ
 function peco-find() {
   local filepath="$(find . | grep -v '/\.' | peco --prompt 'PATH>')"
   [ -z "$filepath" ] && return
-  echo $filepath
   if [ -n "$LBUFFER" ]; then
     BUFFER="$LBUFFER$filepath"
   else
@@ -28,7 +27,7 @@ function peco-ls() {
     BUFFER="$LBUFFER$filepath"
   else
     if [ -d "$filepath" ]; then
-      BUFFER="cd $filepath"
+        BUFFER="cd $(echo $filepath|sed -e 's/ /\\ /g')"
     elif [ -f "$filepath" ]; then
       BUFFER="$EDITOR $filepath"
     fi
