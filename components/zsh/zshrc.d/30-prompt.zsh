a65a7c67f2c9cc6514606d749e1991f500c3f703
Kosuke Kusano <cocuh.kk@gmail.com>
2015-09-25 03:33:10 +0900

    use shasum

--- components/zsh/zshrc.d/30-prompt.zsh
+++ components/zsh/zshrc.d/30-prompt.zsh
@@ -1,6 +1,7 @@
 #zsh PROMPT
 local host_formats;host_formats=('%{${fg[red]}%}' '%{${fg[green]}%}' '%{${fg[cyan]}%}')
-local host_int=$(echo "ibase=16;hostname=$(echo $(hostname) | sha1sum | awk '{print toupper($1)}');ibase=A;hostname%${#host_formats}" | bc)
+local host_int=$(echo "ibase=16;hostname=$(echo $(hostname) | shasum | awk '{print toupper($1)}');ibase=A;hostname%${#host_formats}" | bc)
+echo host_int
 
 local the_prompt="%{${fg_bold[cyan]}%}%n@${host_formats[host_int]}%m%{${reset_color}%}"
 case $(hostname) in
