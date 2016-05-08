caf9d01cc41bf4a036e00b67deda78240d13f212
Kosuke Kusano <cocuh.kk@gmail.com>
2016-05-08 23:52:11 +0900

    fix bugs

--- components/zsh/zshrc.d/90-utils-dotfiles.zsh
+++ components/zsh/zshrc.d/90-utils-dotfiles.zsh
@@ -2,7 +2,17 @@ function dotfiles-update(){
   pwd=$(pwd)
   cd ~/.dotfiles
   git pull --recurse-submodules
-  git submodule update --recursive
+  git submodule foreach 'git pull origin master'
+  git submodule foreach 'git checkout master'
+  cd $pwd
+}
+
+function dotfiles-submodule-update(){
+  pwd=$(pwd)
+  cd ~/.dotfiles
+  git submodule foreach 'git pull origin master'
+  git submodule foreach 'git checkout master'
+  git commit -m 'update submodules'
   cd $pwd
 }
 
