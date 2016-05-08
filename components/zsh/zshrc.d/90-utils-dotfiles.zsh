773eb98f8396b9ea1576801c2e73c24f2a69a6ff
Kosuke Kusano <cocuh.kk@gmail.com>
2016-05-08 23:53:07 +0900

    remove meanless lines

--- components/zsh/zshrc.d/90-utils-dotfiles.zsh
+++ components/zsh/zshrc.d/90-utils-dotfiles.zsh
@@ -7,12 +7,3 @@ function dotfiles-update(){
   cd $pwd
 }
 
-function dotfiles-submodule-update(){
-  pwd=$(pwd)
-  cd ~/.dotfiles
-  git submodule foreach 'git pull origin master'
-  git submodule foreach 'git checkout master'
-  git commit -m 'update submodules'
-  cd $pwd
-}
-
