bc382e81206dc026a5e8f1af6a6153e1a5876542
Kosuke Kusano <cocuh.kk@gmail.com>
2017-03-18 22:40:53 +0900

    update regen script

--- regenzshrc.sh
+++ regenzshrc.sh
@@ -1,17 +1,16 @@
 #!/bin/zsh
-
 function regenzshrc(){
-    rm ~/.zshrc
-    for filename in $(find ~/.zshrc.d/ -type f -name "*.zsh"| sort)
-    do
-        echo \#$(basename $filename) >> ~/.zshrc
-        cat $filename >> ~/.zshrc
-        echo >> ~/.zshrc 
-    done
-    echo regened zshrc
-    source ~/.zshrc
+  rm ~/.zshrc
+  for filename in $(find ~/.zshrc.d/ -type f -name "*.zsh"| sort)
+  do
+    echo '() {' >> ~/.zshrc
+    echo \#$(basename $filename) >> ~/.zshrc
+    cat $filename >> ~/.zshrc
+    echo '}' >> ~/.zshrc
+    echo >> ~/.zshrc 
+  done
+  echo regened zshrc
 }
-
 if [ -e ~/.zshrc ];then
     rm ~/.zshrc
 fi
