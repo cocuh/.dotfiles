5d118a5b0ed6e3a4b9895fbb45df56c844eb16e9
Kosuke Kusano <cocuh.kk@gmail.com>
2017-03-18 22:11:34 +0900

    [zsh] submodule auto update commit

--- components/zsh/zshrc.d/90-utils-dotfiles.zsh
+++ components/zsh/zshrc.d/90-utils-dotfiles.zsh
@@ -1,9 +1,15 @@
 function dotfiles-update(){
   pwd=$(pwd)
   cd ~/.dotfiles
+  git stash
   git pull --recurse-submodules
   git submodule foreach 'git pull origin master'
   git submodule foreach 'git checkout master'
+  git add .
+  git commit -m ':up:submodules'
+  git push
+  git stash pop
   cd $pwd
 }
+
 
