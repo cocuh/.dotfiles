12c8353d8f7fb30dd170481bc82eee6218eb50eb
Kosuke Kusano <cocuh.kk@gmail.com>
2017-07-04 21:36:27 +0900

    notify zsh

---
+++ components/zsh/zshrc.d/extra/term.zsh
@@ -0,0 +1,7 @@
+case "${TERM}" in
+  xterm-termite)
+    alias ssh="TERM=xterm ssh"
+    ;;
+  *)
+    ;;
+esac
---
+++ components/zsh/zshrc.d/utils/notify.zsh
@@ -0,0 +1,14 @@
+function notify_preexec {
+  notify_prev_executed_at=`date`
+  notify_prev_command=$2
+}
+
+function notify_precmd {
+  if [ $TTYIDLE -gt 5 ] && [ $(which notify-send) ]; then
+    notify-send -i ~/.zshrc.d/resources/zsh.svg "<big><b>$notify_prev_command</b></big> <br/>$notify_prev_executed_at"
+  fi
+}
+
+autoload -Uz add-zsh-hook
+add-zsh-hook preexec notify_preexec
+add-zsh-hook precmd notify_precmd
