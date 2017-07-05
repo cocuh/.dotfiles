475e6e34647540a9a81ffb0c10039ef62bfed13a
Kosuke Kusano <cocuh.kk@gmail.com>
2017-07-06 05:54:28 +0900

    no submodule update

--- components/zsh/zshrc.d/utils/dotfiles.zsh
+++ components/zsh/zshrc.d/utils/dotfiles.zsh
@@ -3,9 +3,15 @@ function dotfiles-update(){
   cd ~/.dotfiles
   git stash
   git pull --recurse-submodules
+  git stash pop
+  cd $pwd
+}
+
+function dotfiles-update-submodules(){
+  pwd=$(pwd)
+  cd ~/.dotfiles
   git submodule foreach 'git pull origin master'
   git submodule foreach 'git checkout master'
-  git stash pop
   cd $pwd
 }
 
