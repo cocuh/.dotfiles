ca8f80e46fb53cb1adc96c3cd3d620c52365a1ae
Kosuke Kusano <cocuh.kk@gmail.com>
2017-07-17 19:08:39 +0900

    quite ssh-add

--- components/zsh/zshrc.d/extra/sshagent.zsh
+++ components/zsh/zshrc.d/extra/sshagent.zsh
@@ -7,7 +7,7 @@ if [ $? = 2 ] ; then
     ssh-agent | sed -e 's/^echo/\# echo/g' > ~/.ssh-agent-info
     source ~/.ssh-agent-info
 fi
-if ssh-add -l >&/dev/null ; then
+if ssh-add -l >& /dev/null ; then
 else
-    ssh-add
+    ssh-add >& /dev/null
 fi
