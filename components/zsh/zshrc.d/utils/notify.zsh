9c5f79f8f81305cb8e610978329a13fff33a012c
Kosuke Kusano <cocuh.kk@gmail.com>
2018-05-25 00:49:28 +0900

    fix zsh notify threshold

--- components/zsh/zshrc.d/utils/notify.zsh
+++ components/zsh/zshrc.d/utils/notify.zsh
@@ -6,7 +6,7 @@ function notify_preexec {
 function notify_precmd {
   notify_prev_status=$?
   EXIT_CODE_KEYBOARD_INTERRUPT=130
-  if [ $TTYIDLE -gt 3 ] &&
+  if [ $TTYIDLE -gt 30 ] &&
     [ $notify_prev_status -ne $EXIT_CODE_KEYBOARD_INTERRUPT ] &&
     (type notify-send &> /dev/null); then
     elapsed_time=$(python -c "print((lambda s:'{:.0f}s'.format(s)if s<120 else(lambda m:'{:.1f}m'.format(m)if m<100 else(lambda h:'{:.1f}h'.format(h))(m/60))(s/60))($TTYIDLE))")
