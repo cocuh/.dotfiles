8547d4bf4bfb8142ba325f9daf8cb466a438608b
Kosuke Kusano <cocuh.kk@gmail.com>
2018-05-25 00:48:12 +0900

    fix some bug and show elapsed time

--- components/zsh/zshrc.d/utils/notify.zsh
+++ components/zsh/zshrc.d/utils/notify.zsh
@@ -6,10 +6,11 @@ function notify_preexec {
 function notify_precmd {
   notify_prev_status=$?
   EXIT_CODE_KEYBOARD_INTERRUPT=130
-  if [ $TTYIDLE -gt 30 ] &&
+  if [ $TTYIDLE -gt 3 ] &&
     [ $notify_prev_status -ne $EXIT_CODE_KEYBOARD_INTERRUPT ] &&
     (type notify-send &> /dev/null); then
-    notify-send -i ~/.zshrc.d/resources/zsh.svg "<big><b>Done: $notify_prev_command</b></big> <br/>$notify_prev_executed_at"
+    elapsed_time=$(python -c "print((lambda s:'{:.0f}s'.format(s)if s<120 else(lambda m:'{:.1f}m'.format(m)if m<100 else(lambda h:'{:.1f}h'.format(h))(m/60))(s/60))($TTYIDLE))")
+    notify-send -i ~/.zshrc.d/resources/zsh.svg "Done: $notify_prev_command" "$notify_prev_executed_at ($elapsed_time)"
   fi
 }
 
