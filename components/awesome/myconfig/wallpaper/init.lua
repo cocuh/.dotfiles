efdca24b055418ef1ce518d6a84ab20d9c784a98
Kosuke Kusano <cocuh.kk@gmail.com>
2018-09-11 18:33:58 +0900

    fix bug

--- components/awesome/myconfig/wallpaper/init.lua
+++ components/awesome/myconfig/wallpaper/init.lua
@@ -8,19 +8,24 @@ local home_dir = os.getenv("HOME")
 local wallpapers = {
   stern = {
     default = home_dir .. "/picture/wallpaper/materials.png",
-  }
+  },
+  shiina = {
+    default = home_dir .. "/picture/wallpaper/stella.png",
+  },
 }
 
 local wallpaper_setting = wallpapers[hostname]
 
 function set_wallpaper(path)
-  for s=1, sysconst.screen_num + 1 do
+  for s=1, sysconst.screen_num do
     gears.wallpaper.maximized(path, s)
   end
 end
 
 function set_default_wallpaper()
-  set_wallpaper(wallpaper_setting.default)
+  if wallpaper_setting ~= nil then
+    set_wallpaper(wallpaper_setting.default)
+  end
 end
 
 return {
